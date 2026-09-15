9a56388570f2b8c71362b70e77adbbe2571fa47e
Arthur ASCEDU <arthur.ascedu@hpe.com>
2026-09-15 16:16:43 +0200

    added learning rate and iterations input in CLI, normalized km values for better use during model training, updated model to be able to save result to a json file

--- train.py
+++ train.py
@@ -2,29 +2,59 @@
 import argparse
 import csv
 
+class Model():
+    normalized_km: list[float]
+    price: list[int]
+    m: int
+    iterations: int
+    learning_rate: float
+    km_min: float = 0.0
+    km_max: float = 0.0
+    theta0: float = 0.0
+    theta1: float = 0.0
+
+    def __init__(self, args: argparse.Namespace) -> None:
+        self.normalized_km = []
+        self.price = []
+        reader = csv.DictReader(args.data)
+        km = []
+        for row in reader:
+            try:
+                km.append(int(row['km']))
+                self.price.append(int(row['price']))
+            except:
+                print(f"Data is wrong: {row}")
+                exit()
+        self.m = len(km)
+        self.iterations = args.i
+        self.learning_rate = args.learning
+        self.km_min = min(km)
+        self.km_max = max(km)
+        for x in km:
+            self.normalized_km.append((x - self.km_min) / (self.km_max - self.km_min))
+
+    def train(self) -> None:
+
+        for _ in range(self.iterations):
+            tmp_theta0 = 0.0
+            tmp_theta1 = 0.0
+            for i in range(self.m):
+                print(self.normalized_km[i], self.price[i])
+
+
+
 def parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Train your linear regression model")
     parser.add_argument("--data", required=True, type=argparse.FileType("r"), help="Path to the CSV file")
-    parser.add_argument("-i", default=1000, type=int, help="Number of iterations during training")
+    parser.add_argument("-i", default=1000, type=int, help="Number of iterations during training (default 1000)")
+    parser.add_argument("-l", "--learning", default=0.01, type=float, help="Learning rate used by the gradient descent (default 0.01)")
     args = parser.parse_args()
     return args
 
 def main():
     args = parser()
-    km = []
-    price = []
-    reader = csv.DictReader(args.data)
-    for row in reader:
-        # if row['km'] is None or row["price"] is None or len(row['km']) == 0 or len(row['price']) == 0:
-        #     print(f"Data is malformed: km:{row['km']} / price:{row['price']}")
-        #     return
-        try:
-            km.append(int(row['km']))
-            price.append(int(row['price']))
-        except:
-            print(f"Data is wrong: {row}")
-            exit()
-    m = len(km)
+    model = Model(args)
+    model.train()
 
 if __name__ == "__main__":
     main()
